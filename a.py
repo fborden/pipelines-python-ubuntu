9a14c2fe16054ace3ca5d47071f05bf686bb25f9
Frederick Borden <fborden@cincom.com>
2022-07-28 14:25:38 -0400

    Update a.py

--- a.py
+++ a.py
@@ -10,8 +10,8 @@ sum = float(num1) + float(num2)
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
 # Check the firewall status
-fwstatus = "sudo ufw status"
-print (f"FIrewall status: sudo ufw status")
+fwstatus = ${sudo ufw status}
+print (f"FIrewall status: {fwstatus}")
 
 # Ping an external Windows machine from within the pipeline
 ip_list = ['192.168.0.174', '127.0.0.1', '127.0.1.1', '10.0.2.15', '8.8.8.8', 'google.com']
