ed9c88a129dc46a37d787f76d966c8aa6e9258fd
Frederick Borden <fborden@cincom.com>
2022-07-28 16:59:50 -0400

    Update a.sh

--- a.sh
+++ a.sh
@@ -10,16 +10,16 @@ sum=$(($num1 + $num2))
 echo "The sum of $num1 and $num2 is $sum"
 
 # Check the firewall status
-cmd = `sudo ufw status`
+cmd=`sudo ufw status`
 echo "firewall status: $cmd"
 
 # Ping an external Windows machine from within the pipeline
-ip_list = ("192.168.0.174" "127.0.0.1" "127.0.1.1" "10.0.2.15" "8.8.8.8" "google.com")
-substring = "Destination Host Unreachable"
+ip_list=("192.168.0.174" "127.0.0.1" "127.0.1.1" "10.0.2.15" "8.8.8.8" "google.com")
+substring="Destination Host Unreachable"
 
 for ip in $(ip_list[@])
 do
-    response = `ping $ip -c 4`
+    response=`ping $ip -c 4`
     
     if [ ! $response =~ $substring ]; # substring was not found in response
         echo "UP: $ip Ping Successful"
