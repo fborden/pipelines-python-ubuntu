c6f4f08460f6b4f5ca55040b4a017429235708e9
Frederick Borden <fborden@cincom.com>
2022-07-28 13:08:37 -0400

    Update a.py

--- a.py
+++ a.py
@@ -1,4 +1,6 @@
-num1 =10
+import os
+
+num1 = 10
 num2 = 30
  
 # Add two numbers
@@ -6,3 +8,12 @@ sum = float(num1) + float(num2)
  
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Ping an external Windows machine from within the pipeline
+ip_list = ['192.168.0.174', '127.0.0.1', '127.0.1.1', '10.0.2.15', '8.8.8.8']
+for ip in ip_list:
+    response = os.popen(f"ping {ip} -c 4").read()
+    if "Received = 4" in response:
+        print(f"UP {ip} Ping Successful")
+    else:
+        print(f"DOWN {ip} Ping Unsuccessful")
