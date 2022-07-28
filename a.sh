a4d56c50f6ae0d8ab54d88fec6df49315c312564
Frederick Borden <fborden@cincom.com>
2022-07-28 17:51:00 -0400

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-num1=10
-num2=30
+#num1=10
+#num2=30
  
 # Add two numbers
-sum=$(($num1 + $num2))
+#sum=$(($num1 + $num2))
  
 # Display the sum
-printf "The sum of $num1 and $num2 is $sum\n\n"
+#printf "The sum of $num1 and $num2 is $sum\n\n"
 
 # Check the firewall status
 cmd=`sudo ufw status`
-printf "firewall status:$cmd\n\n"
+printf "Firewall Status: $cmd\n\n"
 
 # Check firwall rules for ICPM
 cmd=`sudo cat /etc/ufw/before.rules | grep -i icmp`
-printf "firewall rules for icmp:\n$cmd"
+printf "firewall rules for icmp:\n$cmd\n\n"
 
 # Ping an external Windows machine from within the pipeline
 ip_list=( 192.168.0.174 127.0.0.1 127.0.1.1 10.0.2.15 8.8.8.8 google.com )
@@ -24,13 +24,13 @@ substring="Destination Host Unreachable"
 for ip in "${ip_list[@]}"
 do
     response=`ping $ip -c 4`
-    printf "Ping response:\n$response"
+    printf "Ping response:\n$response\n"
     
     if [[ "$response" =~ "$substring" ]]
     then
-        printf "DOWN: $ip Ping Unsuccessful"
+        printf "\nDOWN: $ip Ping Unsuccessful"
     else
-        printf "UP: $ip Ping Successful"
+        printf "\nUP: $ip Ping Successful"
     fi
     
 done
