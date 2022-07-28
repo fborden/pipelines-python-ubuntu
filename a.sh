aed535ee0a72402ecedde741754f34b7a237c820
Frederick Borden <fborden@cincom.com>
2022-07-28 18:10:11 -0400

    Update a.sh

--- a.sh
+++ a.sh
@@ -18,14 +18,14 @@ cmd=`sudo cat /etc/ufw/before.rules | grep -i icmp`
 printf "firewall rules for icmp:\n$cmd\n\n"
 
 # Ping an external Windows machine from within the pipeline
-ip_list=( 192.168.0.174 127.0.0.1 127.0.1.1 10.0.2.15 8.8.8.8 google.com 192.168.1.1 )
+ip_list=( 192.168.0.174 127.0.0.1 127.0.1.1 10.0.2.15 8.8.8.8 192.168.1.1 google.com )
 substring="Destination Host Unreachable"
 
 for ip in "${ip_list[@]}"
 do
 
     response=`ping $ip -c 4`
-    printf "Ping response:\n$response\n\n"
+    printf "\n\n$response\n\n"
     
     #if [[ "$response" =~ "$substring" ]]
     #then
