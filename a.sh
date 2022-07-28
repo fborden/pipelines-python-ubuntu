dc79a4719fcfee4b786a3f74d542c5c085d129a8
Frederick Borden <fborden@cincom.com>
2022-07-28 17:14:55 -0400

    Update a.sh

--- a.sh
+++ a.sh
@@ -7,21 +7,21 @@ num2=30
 sum=$(($num1 + $num2))
  
 # Display the sum
-echo "The sum of $num1 and $num2 is $sum"
+echo "The sum of $num1 and $num2 is $sum\n\n"
 
 # Check the firewall status
 cmd=`sudo ufw status`
-echo "firewall status: $cmd"
+echo "firewall status: $cmd\n\n"
 
 # Check firwall rules for ICPM
 cmd=`sudo cat /etc/ufw/before.rules | grep -i icmp`
-echo "firewall rules for icmp: $cmd"
+echo "firewall rules for icmp:\n$cmd"
 
 # Ping an external Windows machine from within the pipeline
 ip_list=("192.168.0.174" "127.0.0.1" "127.0.1.1" "10.0.2.15" "8.8.8.8" "google.com")
 substring="Destination Host Unreachable"
 
-for ip in $(ip_list[@])
+for ip in ${ip_list[@]}
 do
     response=`ping $ip -c 4`
     
