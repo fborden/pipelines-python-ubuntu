a4458833e6557bbff90d3d39f6c7153b114c13f6
Frederick Borden <fborden@cincom.com>
2022-07-29 11:21:23 -0400

    Update a.sh

--- a.sh
+++ a.sh
@@ -15,10 +15,10 @@ printf "\nFirewall Status: $cmd\n\n"
 
 # Check firwall rules for ICPM
 cmd=`sudo cat /etc/ufw/before.rules | grep -i icmp`
-printf "Firewall ules for ICMP/ping:\n$cmd\n\n"
+printf "Firewall rules for ICMP/ping:\n$cmd\n\n"
 
 # Ping an external Windows machine from within the pipeline
-ip_list=( 192.168.0.174 127.0.0.1 127.0.1.1 10.0.2.15 8.8.8.8 192.168.1.1 goole.com)
+ip_list=( 192.168.0.174 127.0.0.1 127.0.1.1 10.0.2.15 8.8.8.8 192.168.1.1 google.com cincom.com )
 substring="Destination Host Unreachable"
 
 for ip in "${ip_list[@]}"
@@ -30,10 +30,12 @@ do
     if [[ $rc -eq 1 ]]
     then
          #printf "\n$response\n"
-         printf "\n\nDOWN: $ip Ping Unsuccessful\n\n"
+         printf "%s\n" ---------------
+         printf "DOWN: $ip Ping Unsuccessful"
     else
         #printf "\n$response\n"
-        printf "\n\nUP: $ip Ping Successful\n\n"
+         printf "%s\n" ---------------
+        printf "UP: $ip Ping Successful"
     fi
     
     #if [[ "$response" =~ "$substring" ]]
